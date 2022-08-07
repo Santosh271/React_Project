5b59f58878f73e27b3f416a8f0bd0098280776ae
unknown <santoshnagpure70000@gmail.com>
2022-08-06 23:15:39 -0700

    Default vs Named Export Explain

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import {Footer} from './Components/Footer';
 
 function App() {
-
+  // root class main folder
   return (
     <>
       <Header/>
-
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+      <h1>Hi, i'm footer here</h1>
+    </div>
+  )
+}
+
+
+
