5d74b3f22df93e65c9f4bf0b914d2245023b47d3
unknown <santoshnagpure70000@gmail.com>
2022-08-06 22:38:08 -0700

    react-first-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+
+    </>
+  );
+}
+
+export default App;
